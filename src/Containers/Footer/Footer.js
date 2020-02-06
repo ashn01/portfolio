ec7fafc1dc2d602960c98ad5dd628386c5661549
Youngmin-mac <midvalue@gmail.com>
2020-02-06 13:52:46 -0500

    Implemented Footer

---
+++ src/Containers/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+import '../../css/Footer.css'
+
+export default class Footer extends React.PureComponent
+{
+
+    render()
+    {
+        return (
+        <div className="footer">
+            <div className="copyright">
+                COPYRIGHT 2020 YOUNGMIN KIM. ALL RIGHTS RESERVED.
+            </div>
+        </div>
+        )
+    }
+}
