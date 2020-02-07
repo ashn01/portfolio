01798115d98f356989c8138d7fd6a36dfcf2a441
Youngmin-mac <midvalue@gmail.com>
2020-02-07 16:22:04 -0500

    Added router

--- src/Components/Body/Projects.js
+++ src/Components/Body/Projects.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import $ from 'jquery'
 
 
 import '../../css/Body.css'
@@ -10,56 +9,7 @@ export default class Projects extends React.PureComponent
     {
         super(props)
         this.state = {
-            projects : [
-                {
-                    id: 0,
-                    name: "dummy project1",
-                    description: "dummy data1",
-                    imgsrc: "",
-                    scale: 4,
-                    priority: 1,
-                },
-                {
-                    id: 1,
-                    name: "dummy project2",
-                    description: "dummy data2",
-                    imgsrc: "",
-                    scale: 2,
-                    priority: 3,
-                },
-                {
-                    id: 2,
-                    name: "dummy project3",
-                    description: "dummy data3",
-                    imgsrc: "",
-                    scale: 1,
-                    priority: 2,
-                },
-                {
-                    id: 3,
-                    name: "dummy project4",
-                    description: "dummy data4",
-                    imgsrc: "",
-                    scale: 2,
-                    priority: 3,
-                },
-                {
-                    id: 4,
-                    name: "dummy project5",
-                    description: "dummy data5",
-                    imgsrc: "",
-                    scale: 1,
-                    priority: 3,
-                },
-                {
-                    id: 5,
-                    name: "dummy project6",
-                    description: "dummy data6",
-                    imgsrc: "",
-                    scale: 2,
-                    priority: 3,
-                },
-            ]
+            projects : []
         }
     }
     componentDidMount()
@@ -78,7 +28,7 @@ export default class Projects extends React.PureComponent
             temp['description'] = "This is about "+temp.name+". Dummy description Hello world Awesome !"
             temp['imgsrc'] = "https://i.stack.imgur.com/GA6bB.png"
             var rand = Math.floor(Math.random()*3)+1;
-            temp['scale']= rand == 3 ? 4 : rand;
+            temp['scale']= rand === 3 ? 4 : rand;
             temp['priority']=Math.floor(Math.random()*4)+1;
             arr.push(temp)
         }
@@ -94,7 +44,7 @@ export default class Projects extends React.PureComponent
                         this.state.projects.sort((a,b)=> a.priority-b.priority).map((v,i)=>{
                             return (
                                 <div key={i} className={`projectScale${v.scale}`}>
-                                    <img src="https://i.stack.imgur.com/GA6bB.png" className="projectImg"/>
+                                    <img src={v.imgsrc} alt={v.description} className="projectImg"/>
                                     <div className="projectItem">
                                         <div>
                                             <div className="projectTitle">
@@ -104,7 +54,7 @@ export default class Projects extends React.PureComponent
                                                 {v.description}
                                             </div>
                                             <div className="projectLink">
-                                                <a href='#' >View Detail</a>
+                                                <a href={`/project#${i}`} >View Detail</a>
                                             </div>
                                         </div>
                                     </div>
