b0427a0da598b3c5fe3062a14114f192b1cd5be4
Youngmin-mac <midvalue@gmail.com>
2020-02-07 15:07:23 -0500

    Implemented project tiles with grid css

--- src/Components/Body/Projects.js
+++ src/Components/Body/Projects.js
@@ -44,142 +44,72 @@ export default class Projects extends React.PureComponent
                     priority: 3,
                 },
                 {
-                    id: 3,
-                    name: "dummy project4",
-                    description: "dummy data4",
-                    imgsrc: "",
-                    scale: 2,
-                    priority: 3,
-                },
-                {
                     id: 4,
                     name: "dummy project5",
                     description: "dummy data5",
                     imgsrc: "",
                     scale: 1,
                     priority: 3,
-                }
-            ],
-            gridArea : [],
-            modified:[]
+                },
+                {
+                    id: 5,
+                    name: "dummy project6",
+                    description: "dummy data6",
+                    imgsrc: "",
+                    scale: 2,
+                    priority: 3,
+                },
+            ]
         }
     }
     componentDidMount()
     {
-        this.createGrid();
+        this.dummydata()
     }
 
-    createGrid()
+    dummydata()
     {
-        var prjs = this.state.projects.sort((a,b)=>a.priority-b.priority)
-        var ret=[];
-        var temp=[];
-        var weight=0;
-        var colPos = 1;
-        var rowPos = 1;
-        while(prjs.length > 0)
+        var arr=[]
+        for(var i=0;i<10;++i)
         {
-            for(var i=0;i<prjs.length;++i)
-            {
-                if(weight+prjs[i].scale <= 6)
-                {
-                    var spanCol, spanRow
-                    switch(prjs[i].scale)
-                    {
-                        case 4:
-                            spanCol = 2;
-                            spanRow = 2;
-                            break;
-                        case 2:
-                            spanCol = 1;
-                            spanRow = 2;
-                            break;
-                        case 1:
-                            spanCol = 1;
-                            spanRow = 1;
-                            break;
-                    }
-                    prjs[i]["gridArea"]=ret.length*2+rowPos+" / "+colPos+" / span "+spanRow+" / span "+spanCol;
-                    temp.push(prjs[i]);
-                    colPos+=spanCol
-                    rowPos = spanRow == 1 ? 2 : 1; // if small block added right before, row position will be 2
-                    weight+=prjs[i].scale;
-                    prjs.splice(i,1);
-                    i--;
-                    if(weight==6)
-                        break;
-                }
-            }
-            ret.push(temp);
-            temp=[];
-            weight=0;
-            colPos=1;
-            rowPos=1;
+            var temp = {}
+            temp['id'] = i
+            temp['name'] = "dummy project "+(i+1)
+            temp['description'] = "This is about "+temp.name+". Dummy description Hello world Awesome !"
+            temp['imgsrc'] = "https://i.stack.imgur.com/GA6bB.png"
+            var rand = Math.floor(Math.random()*3)+1;
+            temp['scale']= rand == 3 ? 4 : rand;
+            temp['priority']=Math.floor(Math.random()*4)+1;
+            arr.push(temp)
         }
-        
-        // var grid="";
-        // var str1="",str2="";
-        // ret.map((v,i)=>{
-        //     v.map((vi,i)=>{
-        //         switch(vi.scale)
-        //         {
-        //             case 4:
-        //                 str1+=vi.id+" " + vi.id+" "
-        //                 str2+=vi.id+" " + vi.id+" "
-        //                 break;
-        //             case 2:
-        //                 str1+=vi.id+" "
-        //                 str2+=vi.id+" "
-        //                 break;
-        //             case 1:
-        //                 var temp = str2.search("dummy")
-        //                 if(str2.search("dummy") != -1)
-        //                     str2 = str2.replace('dummy',vi.id+" ")
-        //                 else
-        //                 {
-        //                     str1+=vi.id+" "
-        //                     str2+="dummy"
-        //                 }
-        //                 break;
-        //         }
-        //     })
-        //     grid+="'"+str1+"'\n"+"'"+str2+"'\n"
-        //     str1=""
-        //     str2=""
-        // })
-        this.setState({ modified:ret},()=>{
-            console.log(this.state.modified)
-        })
-        //$(".projectGridContainer").css("grid-template-areas",grid)
+        this.setState({projects:arr})
     }
 
     render()
     {
         return (
             <div className="projects">
-                <div className="projectGridContainer" style={{display:'grid', gridTemplateColumns: "auto auto auto auto", gridGap:'10px'}}>
+                <div className="projectGridContainer">
                     {
-                        this.state.modified.map((v,i)=>{
-                            return v.map((vi,ii)=>{
-                                return (
-                                    <div key={ii} className="" style={{gridArea:`${vi.gridArea}`, width:'100%', height:'100%', position:'relative'}}>
-                                        <img src="https://i.stack.imgur.com/GA6bB.png" className="projectImg"/>
-                                        <div className="projectItem">
-                                            <div>
-                                                <div className="projectTitle">
-                                                    {vi.name}
-                                                </div>
-                                                <div className="projectDescription">
-                                                    {vi.description}
-                                                </div>
-                                                <div className="projectLink">
-                                                    <a href='#' >View Detail</a>
-                                                </div>
+                        this.state.projects.sort((a,b)=> a.priority-b.priority).map((v,i)=>{
+                            return (
+                                <div key={i} className={`projectScale${v.scale}`}>
+                                    <img src="https://i.stack.imgur.com/GA6bB.png" className="projectImg"/>
+                                    <div className="projectItem">
+                                        <div>
+                                            <div className="projectTitle">
+                                                {v.name}
+                                            </div>
+                                            <div className="projectDescription">
+                                                {v.description}
+                                            </div>
+                                            <div className="projectLink">
+                                                <a href='#' >View Detail</a>
                                             </div>
                                         </div>
                                     </div>
-                                )
-                            })
+                                </div>
+                            )
                         })
                     }
                 </div>
