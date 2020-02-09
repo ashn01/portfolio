91e54fab612b64d05d0066f14ebe0773842f038e
YKStudio <ashn01@naver.com>
2020-02-08 23:43:31 -0500

    Modified active nav, Added logo

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -13,7 +13,7 @@ export default class Home extends React.PureComponent
         return (
             <div>
                 <div className="main">
-                    <Header/>
+                    <Header nav="project"/>
                     <Body/>
                 </div>
                 <Footer/>
