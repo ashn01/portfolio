fead88c7df791840807fc1a7cece0f97370121f9
YKStudio <ashn01@naver.com>
2020-02-20 01:22:48 -0500

    Modified CSS for about page

--- src/Components/Body/AboutContents/AboutDetail.js
+++ src/Components/Body/AboutContents/AboutDetail.js
@@ -37,46 +37,6 @@ export default class AboutDetail extends React.PureComponent
                                 Thank you for visiting Youngmin's portfolio!
                             </p>
                         </div>
-                        <div className="aboutInfo">
-                            <div>
-                                <div className="bar"/>
-                                <div className="aboutSummaryTitles">
-                                    Personality
-                                </div>
-                                <div className="aboutSummaryDescription">
-                                    Team player<br/>
-
-                                </div>
-                            </div>
-                            <div>
-                                <div className="bar"/>
-                                <div className="aboutSummaryTitles">
-                                    Experience
-                                </div>
-                                <div className="aboutSummaryDescription">
-                                    Software developer<br/>
-                                    Automation developer
-                                </div>
-                            </div>
-                            <div>
-                                <div className="bar"/>
-                                <div className="aboutSummaryTitles">
-                                    Period
-                                </div>
-                                <div className="aboutSummaryDescription">
-                                    temp
-                                </div>
-                            </div>
-                            <div>
-                                <div className="bar"/>
-                                <div className="aboutSummaryTitles">
-                                    Skillset
-                                </div>
-                                <div className="aboutSummaryDescription">
-                                    C/C++, C#, Java, Javascript, ReactJS, NodeJS, AWS, Azure, Relational Database, NoSQL, Parallel Programming with Cuda, Android, Objective-C, Swift, Git, Jira, Jenkins, Agile, Network programming, Windows, MacOS, Linux 
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
