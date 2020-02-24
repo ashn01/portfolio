a1a155de1b86e487850a210907507d715a2f9d5f
Youngmin-mac <midvalue@gmail.com>
2020-02-24 15:43:52 -0500

    Fixed panel issue

--- src/Components/Header/Navigation.js
+++ src/Components/Header/Navigation.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom';
 import $ from 'jquery'
 import { Elastic  } from 'react-burgers'
  
@@ -21,7 +20,6 @@ export default class Navigation extends React.PureComponent
         $(".navPanel").toggleClass('active')
         $(".burger").toggleClass('active')
         $("body").toggleClass('active')
-        
     }
 
     render()
@@ -38,29 +36,6 @@ export default class Navigation extends React.PureComponent
                         onClick={()=>this.expandMenu()}
                         active={this.state.toggleMenu}
                     />
-                </div>
-                <div className="navPanel">
-                    <ul className="navStyle">
-                        <li className="navItem">
-                            <NavLink to='/home' isActive={(match,location)=>{
-                                return match || location.pathname.search("project") !== -1;
-                                }} activeClassName="active" onClick={()=>this.expandMenu()}>PROJECTS</NavLink>
-                        </li>
-                        <li className="navItem" >
-                            <NavLink to='/about' activeClassName="active" onClick={()=>this.expandMenu()}>ABOUT</NavLink>
-                        </li>
-                        <li className="navItem">
-                            <a href={process.env.PUBLIC_URL + "/images/YMK_resume.pdf"} target="_blank" onClick={()=>this.expandMenu()}>RESUME</a>
-                        </li>
-                    </ul>
-                    <div className="navIcons">
-                        <a href="https://www.linkedin.com/in/youngmin-kim-429b88140" target="_blank" rel="noopener noreferrer">
-                            <img src={process.env.PUBLIC_URL + "/linkedinNav.png"} alt="linkedin"/>
-                        </a>
-                        <a href="https://github.com/ashn01" target="_blank" rel="noopener noreferrer">
-                            <img src={process.env.PUBLIC_URL + "/githubNav.png"} alt="github"/>
-                        </a>
-                    </div>
                 </div>
             </div>
         )
--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import $ from 'jquery'
+import { NavLink } from 'react-router-dom';
 
 import Header from './Header/Header'
 import Home from './Body/Home'
@@ -21,11 +23,42 @@ export default class MainContainer extends React.PureComponent
             default : return null
         }
     }
+
+    expandMenu()
+    {
+        $(".navPanel").toggleClass('active')
+        $(".burger").toggleClass('active')
+        $("body").toggleClass('active')
+    }
+
     render()
     {
         const contents = this.renderWhat()
         return (
             <div>
+                <div className="navPanel">
+                    <ul className="navStyle">
+                        <li className="navItem">
+                            <NavLink to='/home' isActive={(match,location)=>{
+                                return match || location.pathname.search("project") !== -1;
+                                }} activeClassName="active" onClick={()=>this.expandMenu()}>PROJECTS</NavLink>
+                        </li>
+                        <li className="navItem" >
+                            <NavLink to='/about' activeClassName="active" onClick={()=>this.expandMenu()}>ABOUT</NavLink>
+                        </li>
+                        <li className="navItem">
+                            <a href={process.env.PUBLIC_URL + "/images/YMK_resume.pdf"} target="_blank" onClick={()=>this.expandMenu()}>RESUME</a>
+                        </li>
+                    </ul>
+                    <div className="navIcons">
+                        <a href="https://www.linkedin.com/in/youngmin-kim-429b88140" target="_blank" rel="noopener noreferrer">
+                            <img src={process.env.PUBLIC_URL + "/linkedinNav.png"} alt="linkedin"/>
+                        </a>
+                        <a href="https://github.com/ashn01" target="_blank" rel="noopener noreferrer">
+                            <img src={process.env.PUBLIC_URL + "/githubNav.png"} alt="github"/>
+                        </a>
+                    </div>
+                </div>
                 <Header/>
                 <div className="main">
                     {
